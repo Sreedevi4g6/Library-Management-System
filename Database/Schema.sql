10a6dba22546e96fdb0bd0ea8c1e708b7402c67f
Sreedevi4g6 <beere.sreedevi2003@gmail.com>
2025-11-09 15:01:34 +0200

    Create Schema.sql

---
+++ Database/Schema.sql
@@ -0,0 +1,7 @@
+-- Database and tables creation only
+CREATE DATABASE library_management;
+USE library_management;
+
+CREATE TABLE authors (...);
+CREATE TABLE publishers (...);
+-- ... all table creations
